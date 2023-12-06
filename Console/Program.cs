ef1fc7917bf4989a6400f2b770c514708e40be5b
Amira_et65 <Amira_et65@STHQ0118-06>
2023-12-06 20:57:55 +0400

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+
+using System.Drawing.Imaging;
+
+using System.Drawing;
+
+using System.Net.Sockets;
+
+using System.Net;
+ 
+ 
+#pragma warning disable
+
+byte[] CaptureAndSaveScreenshot()
+
+{
+
+    int width = 1920;
+
+    int height = 1080;
+
+    using (Bitmap bmp = new Bitmap(width, height))
+
+    {
+
+        using (Graphics g = Graphics.FromImage(bmp))
+
+        {
+
+            g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+
+        }
+
+        using (MemoryStream stream = new MemoryStream())
+
+        {
+
+            bmp.Save(stream, ImageFormat.Jpeg);
+
+            return stream.ToArray();
+
+        }
+
+    }
+
+}
+
+
+UdpClient udpClient = new UdpClient(27001);
+
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+
+{
+
+    var result = await udpClient.ReceiveAsync();
+
+    remoteEP = result.RemoteEndPoint;
+
+    new Task(async () =>
+
+    {
+
+        await Console.Out.WriteLineAsync(remoteEP.ToString());
+
+        var screen = CaptureAndSaveScreenshot();
+
+        var chunks = screen.Chunk(ushort.MaxValue - 29);
+
+        foreach (var chunk in chunks)
+
+        {
+
+            await udpClient.SendAsync(chunk, chunk.Length, remoteEP);
+
+        }
+
+    }).Start();
+
+
+}
---
+++ ScreenShoot/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenShoot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        BitmapImage ByteToImage(byte[] byteArray)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+
+
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = stream;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            }
+            return bitmapImage;
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            UdpClient client = new UdpClient();
+            var remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27001);
+            var buffer = new byte[ushort.MaxValue - 29];
+            await client.SendAsync(buffer, buffer.Length, remoteEP);
+            var list = new List<byte>();
+            var maxlen = buffer.Length;
+            var len = 0;
+            while (true)
+            {
+                var result = await client.ReceiveAsync();
+                buffer = result.Buffer;
+                len = buffer.Length;
+                list.AddRange(buffer);
+                if (len != maxlen) break;
+            }
+
+            ClientImage.Source = ByteToImage(list.ToArray());
+            list.Clear();
+        }
+    }
+}
+
